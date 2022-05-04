5eacc1012bcb4c725eb30ddf44ae974aa8878e10
Aldi Zainafif <aldi.zainafif@gmail.com>
2022-05-04 18:12:55 +0700

    Allow delete method to cors

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module WakafApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
       end
     end
 
